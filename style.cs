f22b2dc0e5722851f83f3fe3f71f1a26d5a61a33
Tapasya Beniwal <145277974+tapasyabeniwal@users.noreply.github.com>
2024-10-01 18:13:34 +0530

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,37 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 1em;
+    text-align: center;
+}
+
+nav ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: space-between;
+}
+
+nav li {
+    margin-right: 20px;
+}
+
+main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2em;
+}
+
+#feed {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
